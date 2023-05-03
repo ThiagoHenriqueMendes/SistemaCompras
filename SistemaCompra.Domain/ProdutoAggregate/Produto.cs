5f2cff03cfdfa713ca49d37453d23be4b4f1ef09
thiago.mendes <thiago.mendes@sellersfunding.com>
2023-05-03 12:17:10 -0300

    Add project files.

---
+++ SistemaCompra.Domain/ProdutoAggregate/Produto.cs
@@ -0,0 +1,54 @@
+﻿using SistemaCompra.Domain.Core;
+using SistemaCompra.Domain.Core.Model;
+using SistemaCompra.Domain.ProdutoAggregate.Events;
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SistemaCompra.Domain.ProdutoAggregate
+{
+    public class Produto : Entity
+    {
+        public Categoria Categoria { get; private set; }
+
+        [NotMapped]
+        public Money PrecoProduto => new Money(Preco);
+
+        public decimal Preco { get; private set; }
+        
+        public string Descricao { get; private set; }
+        public string Nome { get; private set; }
+
+        public Situacao Situacao { get; private set; }
+
+        private Produto() { }
+
+        public Produto(string nome, string descricao, string categoria, decimal preco)
+        {
+            Id = Guid.NewGuid();
+            Nome = nome ?? throw new ArgumentNullException(nameof(nome));
+            Descricao = descricao ?? throw new ArgumentNullException(nameof(descricao));
+            Preco = preco;
+            Categoria = (Categoria)Enum.Parse(typeof(Categoria), categoria);
+            Situacao = Situacao.Ativo;
+        }
+
+        public void Ativar()
+        {
+            Situacao = Situacao.Ativo;
+        }
+
+        public void Suspender()
+        {
+            Situacao = Situacao.Suspenso;
+        }
+
+        public void AtualizarPreco(decimal preco)
+        {
+            if (Situacao != Situacao.Ativo) throw new BusinessRuleException("Produto deve estar ativo!");
+
+            Preco = preco;
+
+            AddEvent(new PrecoAtualizadoEvent(Id, PrecoProduto.Value));
+        }
+    }
+}
