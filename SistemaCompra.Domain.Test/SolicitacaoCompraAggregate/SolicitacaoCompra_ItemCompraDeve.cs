14fec3abd308ec87b5d90c7b95a17c70a2fcb260
thiago.mendes <thiago.mendes@sellersfunding.com>
2023-05-04 11:56:10 -0300

    incluindo testes unitários

---
+++ SistemaCompra.Domain.Test/SolicitacaoCompraAggregate/SolicitacaoCompra_ItemCompraDeve.cs
@@ -0,0 +1,49 @@
+﻿using Bogus;
+using SistemaCompra.Domain.Core;
+using SistemaCompra.Domain.ProdutoAggregate;
+using SistemaCompra.Domain.SolicitacaoCompraAggregate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace SistemaCompra.Domain.Test.SolicitacaoCompraAggregate
+{
+    public class SolicitacaoCompra_ItemCompraDeve
+    {
+        private readonly Faker _faker;
+
+        public SolicitacaoCompra_ItemCompraDeve()
+        {
+            _faker = new Faker();
+        }
+
+        [Fact]
+        public void NotificarErroQuandoNaoInformarItensCompra()
+        {
+            //Dado
+            var solicitacao = new SolicitacaoCompra("rodrigoasth", "rodrigoasth");
+            var itens = new List<Item>();
+
+            //Quando 
+            var ex = Assert.Throws<BusinessRuleException>(() => solicitacao.RegistrarCompra(itens));
+
+            //Então
+            Assert.Equal("A solicitação de compra deve possuir itens!", ex.Message);
+        }
+
+        [Fact]
+        public void DefinirCalculoSubTotal()
+        {
+            //Dado
+            var produto = new Produto("Eucalipto", "Eucalipto", Categoria.Madeira.ToString(), _faker.Random.Decimal(0.1M, 50000));
+            var itemProduto = new Item(produto, _faker.Random.Int());
+
+            //Quando 
+            var calculo = itemProduto.Produto.Preco.Value * itemProduto.Qtde;
+
+            //Então
+            Assert.Equal(calculo, itemProduto.Subtotal.Value);
+        }
+    }
+}
---
+++ SistemaCompra.Domain.Test/SolicitacaoCompraAggregate/SolicitacaoCompra_ProdutoCompraDeve.cs
@@ -0,0 +1,28 @@
+﻿using SistemaCompra.Domain.Core;
+using SistemaCompra.Domain.ProdutoAggregate;
+using SistemaCompra.Domain.SolicitacaoCompraAggregate;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace SistemaCompra.Domain.Test.SolicitacaoCompraAggregate
+{
+    public class SolicitacaoCompra_ProdutoCompraDeve
+    {
+
+        [Fact]
+        public void NotificarErroQuandoNaoInformarProdutoCompra()
+        {
+            //Dado
+            var itens = new List<Item>();
+
+            //Quando 
+            var ex = Assert.Throws<ArgumentNullException>(() => itens.Add(new Item(null, 0)));
+
+            //Então
+            Assert.IsType(typeof(ArgumentNullException), ex);
+        }
+    }
+}
